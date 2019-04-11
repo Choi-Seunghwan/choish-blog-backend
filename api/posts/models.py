72cb691c8fa506f9570841835df65be42d8f1122
Choi Seunghwan <choiseunghwan.tech@gmail.com>
2019-04-11 20:58:11 +0900

    posts model add subtitle field

--- api/posts/models.py
+++ api/posts/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    subtitle = models.CharField(max_length=400)
     contents = models.TextField()
     tags = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
--- api/posts/serializers.py
+++ api/posts/serializers.py
@@ -7,5 +7,5 @@ class PostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Post
         
-        fields = ('id', 'title', 'contents', 'tags', 'created_at', 'update_at',)
+        fields = ('id', 'title', 'subtitle','contents', 'tags', 'created_at', 'update_at',)
         read_only_fields = ('created_at', 'update_at',)
