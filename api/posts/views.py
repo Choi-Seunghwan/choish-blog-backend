956c1b05537d94d1f46bb35badace3237f08e736
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2019-05-09 18:52:07 +0900

    modify views.py(authentication credentials)

--- api/posts/views.py
+++ api/posts/views.py
@@ -3,11 +3,15 @@ from .models import Post
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
 # Create your views here.
 
+
 #temp
-from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework.response import Response
+# from rest_framework import status
 #/temp
 
 
@@ -24,7 +28,7 @@ class PostRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     lookup_field = 'slug'
     
 
-
+@permission_classes((IsAuthenticated, )) 
 class PostCreate(CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
