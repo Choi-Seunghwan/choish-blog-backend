138a3e123980d2c7a0106e66557ccfe695082bce
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2019-05-11 00:37:07 +0900

    modify url.py for getting multi url query

--- api/posts/urls.py
+++ api/posts/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import PostList, PostRetrieveUpdateDestroy, PostCreate
 
 
 
 urlpatterns = [
-    path('', PostList.as_view(), name='post_list'),
+    re_path(r'^$', PostList.as_view(), name='post_list'),
     path('post/<slug:slug>/', PostRetrieveUpdateDestroy.as_view(), name='post_detail'),
     path('new/', PostCreate.as_view(), name='post_create'),
 
